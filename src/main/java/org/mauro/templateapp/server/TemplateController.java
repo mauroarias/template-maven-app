fd47841e2540771ad46b1a7d49938191a5f32dbc
mauro arias <mauro.arias.a@gmail.com>
2022-03-10 18:40:10 -0500

    firts draft

---
+++ src/main/java/org/mauro/templateapp/server/TemplateController.java
@@ -0,0 +1,27 @@
+package org.mauro.templateapp.server;
+
+import org.mauro.templateapp.service.TemplateService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.http.HttpStatus.OK;
+
+@RestController
+@RequestMapping(value = "template/v1")
+public final class TemplateController {
+
+	private final TemplateService templateService;
+
+	@Autowired
+	TemplateController(final TemplateService templateService) {
+		this.templateService = templateService;
+	}
+
+	@GetMapping(value = "/ping", produces = "application/json")
+	ResponseEntity ping() {
+		return new ResponseEntity<>(templateService.ping(), OK);
+	}
+}
