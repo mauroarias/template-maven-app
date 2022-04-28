f0515c5b42b4c9013bc8b10f74cb80d9addd4e40
Mauro Arias <mauroarias@users.noreply.github.com>
2022-04-28 18:33:06 -0400

    closing 0.1.0 (#2)

--- prepare.sh
+++ prepare.sh
@@ -1,37 +1,43 @@
 #!/bin/bash
 
+serviceName=$1
+
 move_package () {
+	package2Move=$1
 	mkdir -p temp
-	cp -rf src/$1/java/org/mauro/templateapp/* temp/
-	mkdir -p src/$1/java/org/mauro/$packageDir
-	cp -rf temp/* src/$1/java/org/mauro/$packageDir/
-	rm -rf src/$1/java/org/mauro/templateapp
+	cp -rf src/$package2Move/java/org/mauro/templateapp/* temp/
+	mkdir -p src/$package2Move/java/org/mauro/$packageDir
+	cp -rf temp/* src/$package2Move/java/org/mauro/$packageDir/
+	rm -rf src/$package2Move/java/org/mauro/templateapp
 	rm -rf temp
-	find "src/$1/java/org/mauro/$packageDir" -type f -name "*.java" -exec sed -i'' -e "s/templateapp/$packageName/g" {} +
+	find "src/$$package2Move/java/org/mauro/$packageDir" -type f -name "*.java" -exec sed -i'' -e "s/templateapp/$packageName/g" {} +
 }
 
 spinal_to_upper () {
-    IFS=- read -ra str <<<"$1"
+    IFS=- read -ra str <<<"$serviceName"
     printf '%s' "${str[@]^}"
 }
 
-appName=$(echo "$1" | sed -e 's/\(.*\)/\L\1/')
+appName=$(echo "$serviceName" | sed -e 's/\(.*\)/\L\1/')
 packageName=$(echo $appName | sed -r "s/-/./g")
 packageDir=$(echo $appName | sed -e "s/-/\//g")
 mainClassName=$(spinal_to_upper $appName)
 appNameCamel=$(echo $mainClassName | sed -e 's/./\L&/')
 test=$(spinal_to_upper $appName)
 
-echo "entry $1"
+echo "entry $serviceName"
 echo "appName $appName"
 echo "packageName $packageName"
 echo "package $packageDir"
 echo "mainClassName $mainClassName"
 echo "appNameCamel $appNameCamel"
 
-cd template-maven-app
 sed -i "s!template-maven-app!$appName!g" pom.xml Dockerfile README.md
 sed -i "s!templateApp!$appNameCamel!g" Dockerfile
+cd ./src
+find . -type f -exec sed -i "s!org.mauro.templateapp!org.mauro.$packageName!g" {} +
+cd ..
+sed -i "s!org.mauro.templateapp!org.mauro.$packageName!g" pom.xml
 
 move_package test
 move_package main
@@ -41,5 +47,3 @@ sed -i "s/TemplateApp/${mainClassName}App/g" "src/main/java/org/mauro/$packageDi
 sed -i "s!template-app!$appName!g" "src/main/resources/application.yml"
 
 mvn clean package
-
-cd ..
